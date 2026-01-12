9c05e6527b3acd3a7fcf618bc1728aba600a8344
Wayne Brown <w.brown.040606@gmail.com>
2026-01-12 18:22:15 +0000

    Created the database. Made the Attendee, Event, User, and Venue models and their respective migrations.

---
+++ sss-home-assignment/app/Models/Event.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'start_datetime',
+        'end_datetime',
+        'status',
+        'organizer_id',
+        'venue_id'
+    ];
+
+    public function organizer(){
+        return $this->belongsTo(User::class, 'organizer_id');
+    }
+
+    public function venue(){
+        return $this->belongsTo(Venue::class, 'venue_id');
+    }
+
+    public function attendees(){
+        return $this->belongsToMany(User::class, 'attendees', 'event_id', 'user_id')
+                    ->withTimestamps();
+    }
+
+    public function attendeeRecords(){
+        return $this->hasMany(Attendee::class, 'event_id');
+    }
+}
